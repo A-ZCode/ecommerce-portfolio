6448bf8e3dbdfade3d06bba8880620be0fe122b7
Zweli <zwelibillionaire@gmail.com>
2025-07-22 18:03:06 +0200

    Initial commit: My e-commerce contributions (BagPanel, Checkout, cartSlice)

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,13 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  items: [],
+  items: [], //Array to hold cart items
 };
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+
+    //Add products to cart
     addItem: (state, action) => {
       const existingItem = state.items.find(
         item => item.id === action.payload.id
@@ -16,13 +18,16 @@ export const cartSlice = createSlice({
         ? existingItem.quantity++ 
         : state.items.push({ ...action.payload, quantity: 1 });
     },
+    //Decrease quantity
     removeItem: (state, action) => {
       state.items = state.items.filter(item => item.id !== action.payload);
     },
+    
     updateQuantity: (state, action) => {
       const item = state.items.find(item => item.id === action.payload.id);
       if (item) item.quantity = action.payload.quantity;
     },
+    //Clear all items from the cart
     clearCart: (state) => {
       state.items = [];
     }
